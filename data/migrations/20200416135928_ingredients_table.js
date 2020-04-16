8860d3c8efeb09329145890fd48603f8ac209cda
Harrison <hmseaborn@gmail.com>
2020-04-16 14:05:44 -0700

    ingrediients_table migration
    knex migrate:make ingredients_table

---
+++ data/migrations/20200416135928_ingredients_table.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('ingredients', function(ingredients) {
+   ingredients.increments();
+   ingredients.string('name').nullable();
+  })
+  
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('ingredients')
+};
