79bf7335dff6500d711c8530282ec6a25f3fee29
Harrison <hmseaborn@gmail.com>
2020-04-16 14:11:13 -0700

    Create 002-ingredients.js

---
+++ data/seeds/002-ingredients.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('ingredients').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('ingredients').insert([
+        { name: 'bread'},
+        { name: 'butter'},
+        { name: 'peanut butter'},
+        { name: 'jelly'},
+        { name: 'cinnamon surgar mix'}
+      ]);
+    });
+};
