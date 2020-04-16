d593373ac6424377377671d0337bb0b480ced5bb
Harrison <hmseaborn@gmail.com>
2020-04-16 15:26:54 -0700

    Create 004-recipe-ingriedients.js
    made a seed file correctly i hope

---
+++ data/seeds/004-recipe-ingriedients.js
@@ -0,0 +1,17 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('recipe_ingredients').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('recipe_ingredients').insert([
+        {recipe_id: 4, ingredient_id: 1, quantity: 2, unit: 'pieces'},
+        {recipe_id: 5, ingredient_id: 1, quantity: 2, unit: 'pieces'},
+        {recipe_id: 5, ingredient_id: 3, quantity: 1, unit: 'preference'},
+        {recipe_id: 5, ingredient_id: 4, quantity: 1, unit: 'preference'},
+        {recipe_id: 6, ingredient_id: 1, quantity: 2, unit: 'pieces'},
+        {recipe_id: 6, ingredient_id: 2, quantity: 2.5, unit: 'tablespoons or preference'},
+        {recipe_id: 6, ingredient_id: 5, quantity: 3.5, unit: 'tablespoons or preference'}
+      ])
+    });
+};
