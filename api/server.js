18fa832fa56a6261b025be9b757edd9a701a767b
Harrison <hmseaborn@gmail.com>
2020-04-16 15:53:24 -0700

    recipesRouter added to server file

--- api/server.js
+++ api/server.js
@@ -1,14 +1,13 @@
 const express = require("express");
-
 const server = express();
+const recipesRouter = require('../recipes/recipes-router')
 
 server.use(express.json());
-
-
-
 
 server.get('/', (req, res) => {
   res.send('<h2>THE SERVER IS UP AND RUNNING TAKE A BREAK YOU HAVE COME SO FAR</h2>')
 })
+
+server.use('/api/recipes', recipesRouter)
 
 module.exports = server;
